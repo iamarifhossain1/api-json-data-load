b08b833fb0f5ac5d67026a48643319f3e4954c65
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-06 23:26:54 +0600

    fetch

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-console.log("js connected");
-
-const person = {
-  name: "Arif Hossain",
-  age: 28,
-  sex: "Male",
-  eduacation: "MBA",
-  occupation: "Unemployed",
-  friends: ["Rony", "Rafel", "Hossain", "Habib", "Rubel", "Shamim", "Shumon"],
-  isRich: false,
-  money: 10000,
+const loadData = (data) => {
+  const result = fetch("https://jsonplaceholder.typicode.com/todos/1")
+    //promise of response
+    .then((response) => response.json())
+    //promise of json data
+    .then((data) => console.log(data));
 };
 
-console.log(typeof person, person);
+const loadPost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      displayPost(data);
+    });
+};
 
-// JSON --> JS Object with Notation
-// JSON.stringify --> Object to JSON
-// JSON.Parse --> JSON to Object
+const displayPost = (posts) => {
+  posts.forEach((posts) => {
+    console.log(posts);
+  });
+};
 
-const personJSON = JSON.stringify(person);
-console.log(typeof personJSON, personJSON);
+// console.log("js connected");
 
-const jsonParse = JSON.parse(personJSON);
-console.log(typeof jsonParse, jsonParse);
+// const person = {
+//   name: "Arif Hossain",
+//   age: 28,
+//   sex: "Male",
+//   eduacation: "MBA",
+//   occupation: "Unemployed",
+//   friends: ["Rony", "Rafel", "Hossain", "Habib", "Rubel", "Shamim", "Shumon"],
+//   isRich: false,
+//   money: 10000,
+// };
+
+// console.log(typeof person, person);
+
+// // JSON --> JS Object with Notation
+// // JSON.stringify --> Object to JSON
+// // JSON.Parse --> JSON to Object
+
+// const personJSON = JSON.stringify(person);
+// console.log(typeof personJSON, personJSON);
+
+// const jsonParse = JSON.parse(personJSON);
+// console.log(typeof jsonParse, jsonParse);
