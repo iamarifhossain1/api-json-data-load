517e680efa6721a0371a173f948c40309288e4ed
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-07 03:07:49 +0600

    fetch

--- posts.js
+++ posts.js
@@ -3,16 +3,28 @@ const loadPost = () => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => displayPost(data));
-
-  const displayPost = (posts) => {
-    // get the container
-    const postContainer = document.getElementById("post-container");
-
-    posts.forEach((post) => {
-      // create an element
-      const li = document.createElement("li");
-      li.innerText = post.title;
-      postContainer.appendChild(li);
-    });
-  };
 };
+
+const displayPost = (posts) => {
+  //1. get the container
+  const postContainer = document.getElementById("post-container");
+  postContainer.innerHTML = "";
+  posts.forEach((post) => {
+    //2. create an element
+    const postCard = document.createElement("div");
+    postCard.innerHTML = `
+    <div class="post-card">
+        <h3>${post.title}{</h3>
+        <p>
+          ${post.body}
+        </p>
+      </div>
+    
+    `;
+
+    //3. add to the container (appendChild)
+    postContainer.appendChild(postCard);
+  });
+};
+
+loadPost();
